ce020a11a55f7c1d3cab15ef2c7d7951dc7fec67
Timmy <mr.oshnokov@gmail.com>
2020-04-16 20:57:29 +0300

    CreateParser #2

--- src/main/kotlin/org.bois/TagsStruct.kt
+++ src/main/kotlin/org.bois/TagsStruct.kt
@@ -1,22 +1,28 @@
+package org.bois
+
 public class TagsStruct {
-    var c;
-    var code;
-    var exmaple;
-    var exception;
-    var include;
-    var list;
-    var para;
-    var param;
-    var paramref;
-    var permission;
-    var inheritdoc;
-    var see;
-    var seealso;
-    var summary;
-    var typeparam;
-    var typeparamref;
-    var returns;
-    var value;
+    var c: String? = null
+    var code: String? = null
+    var example: String? = null
+    var exception: String? = null
+    var include: String? = null
+    var list: String? = null
+    var para: String? = null
+    var param: String? = null
+    var paramref: String? = null
+    var permission: String? = null
+    var inheritdoc: String? = null
+    var see: String? = null
+    var seealso: String? = null
+    var summary: String? = null
+    var typeparam: String? = null
+    var typeparamref: String? = null
+    var returns: String? = null
+    var value: String? = null
 
-
+    override fun toString(): String {
+        return "<c>\t$c\n" +
+                "<code>\t$code\n" +
+                "<example>\t$example\n"
+    }
 }
