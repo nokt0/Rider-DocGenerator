95f3b219e239dde9de762a2fc9535dea8d5f8863
Timmy <mr.oshnokov@gmail.com>
2020-04-16 18:43:17 +0300

    gitflow-feature-stash: CreateParser

---
+++ src/main/resources/org.bois/TagsStruct.kt
@@ -0,0 +1,20 @@
+class TagsStruct {
+    var c;
+    var code;
+    var exmaple;
+    var exception;
+    var include;
+    var list;
+    var para;
+    var param;
+    var paramref;
+    var permission;
+    var inheritdoc;
+    var see;
+    var seealso;
+    var summary;
+    var typeparam;
+    var typeparamref;
+    var returns;
+    var value;
+}
