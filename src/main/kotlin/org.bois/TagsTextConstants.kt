1a8bebbfa5ea23244610d65448b6c1ecf1fe85b7
vladislav <vlad60870@gmail.com>
2020-04-16 21:08:54 +0300

    Added constants

---
+++ src/main/kotlin/org.bois/TagsTextConstants.kt
@@ -0,0 +1,22 @@
+package org.bois
+
+public class TagsTextConstants{
+    val c = "<c>"
+    val code = "<code>"
+    val example = "<example>"
+    val exception = "<exception>"
+    val include = "<include>"
+    val list = "<list>"
+    val para = "<para>"
+    val param = "<param>"
+    val paramref = "<paramref>"
+    val permission = "<permission>"
+    val inheritdoc = "<inheritdoc>"
+    val see = "<see>"
+    val seealso = "<seealso>"
+    val summary = "<summary>"
+    val typeparam = "<typeparam>"
+    val typeparamref = "<typeparamref>"
+    val returns = "<returns>"
+    val value = "<value>"
+}
