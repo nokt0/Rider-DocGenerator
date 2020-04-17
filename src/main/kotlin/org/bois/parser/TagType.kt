3e7baeb597182a35a15418975f08a1e238209ba8
vladislav <vlad6087@gmail.com>
2020-04-18 01:08:07 +0300

    Added new constants

--- src/main/kotlin/org/bois/parser/TagType.kt
+++ src/main/kotlin/org/bois/parser/TagType.kt
@@ -1,22 +1,24 @@
 package org.bois.parser
 
-enum class TagType {
-    C,
-    CODE,
-    EXAMPLE,
-    EXCEPTION,
-    INCLUDE,
-    LIST,
-    PARA,
-    PARAM,
-    PARAMREF,
-    PERMISSION,
-    INHERITDOC,
-    SEE,
-    SEEALSO,
-    SUMMARY,
-    TYPEPARAM,
-    TYPEPARAMREF,
-    RETURNS,
-    VALUE
+enum class TagType(val tag: String) {
+    C("c"),
+    CODE("code"),
+    EXAMPLE("example"),
+    EXCEPTION("exception"),
+    INCLUDE("include"),
+    LIST("list"),
+    PARA("para"),
+    PARAM("param"),
+    PARAMREF("paramref"),
+    PERMISSION("permission"),
+    INHERITDOC("inheritdoc"),
+    SEE("see"),
+    SEEALSO("seealso"),
+    SUMMARY("summary"),
+    TYPEPARAM("typeparam"),
+    TYPEPARAMREF("typeparamref"),
+    RETURNS("returns"),
+    VALUE("value"),
+    JUST_TEXT("just_text"),
+    INIT_TAG("init_tag")
 }
