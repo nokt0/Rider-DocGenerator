170badc6a66c6d1a48b13906290a195389a9e706
vladislav <vlad6087@gmail.com>
2020-04-18 00:51:03 +0300

    Added Tag Class

---
+++ src/main/kotlin/org/bois/parser/TagType.kt
@@ -0,0 +1,22 @@
+package org.bois.parser
+
+enum class TagType {
+    C,
+    CODE,
+    EXAMPLE,
+    EXCEPTION,
+    INCLUDE,
+    LIST,
+    PARA,
+    PARAM,
+    PARAMREF,
+    PERMISSION,
+    INHERITDOC,
+    SEE,
+    SEEALSO,
+    SUMMARY,
+    TYPEPARAM,
+    TYPEPARAMREF,
+    RETURNS,
+    VALUE
+}
