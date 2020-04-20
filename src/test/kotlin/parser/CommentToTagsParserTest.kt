3095e263464cf2ac71c3e5a7cfc407c0c20e4f02
vladislav <vlad60870@gmail.com>
2020-04-20 21:09:42 +0300

    fixed CreateBlocks

--- src/test/kotlin/parser/CommentToTagsParserTest.kt
+++ src/test/kotlin/parser/CommentToTagsParserTest.kt
@@ -10,15 +10,18 @@ import java.nio.file.Paths
 internal class CommentToTagsParserTest {
     @Test
     fun example() {
-        val reader = BufferedReader(FileReader("/Users/napenshe/IdeaProjects/IDEA PROJECT/Rider-DocGenerator/src/test/kotlin/parser/sourceCode.cs"))
+        val reader =
+            BufferedReader(FileReader("/Users/napenshe/IdeaProjects/IDEA PROJECT/Rider-DocGenerator/src/test/kotlin/parser/sourceCode.cs"))
         val parser = CommentToTagsParser(reader)
-        val str  = StringBuffer("<c> Hello World <c>")
+        val str = StringBuffer("<c> Hello World <c>")
         parser.parse(str)
         println(parser.tags)
     }
+
     @Test
-    fun createBlocks(){
-        val reader = BufferedReader(FileReader("/Users/napenshe/IdeaProjects/IDEA PROJECT/Rider-DocGenerator/src/test/kotlin/parser/sourceCode.cs"))
+    fun createBlocks() {
+        val reader =
+            BufferedReader(FileReader("src/test/kotlin/parser/sourceCode.cs"))
         val parser = CommentToTagsParser(reader)
         val result = parser.createBlocks()
         println(result)
