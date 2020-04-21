c1ceff396f4179a700c71e86884cae19a01acc44
vladislav <vlad6087@gmail.com>
2020-04-21 17:16:50 +0300

    fixed ParsedBlockData

--- src/main/kotlin/org/bois/parser/ParsedBlockData.kt
+++ src/main/kotlin/org/bois/parser/ParsedBlockData.kt
@@ -1,6 +1,7 @@
 package org.bois.parser
 
-data class ParsedBlockData(val docs: ArrayList<String>, val header: HeaderType) {
+data class ParsedBlockData(val docs: ArrayList<String>, val header: String) {
+    var type: HeaderType? = null
     override fun toString(): String {
         val string = StringBuffer()
         docs.forEach { string.append(it) }
