ac0aafd3b3d282bade38f71d45349a68f18f00ca
vladislav <vlad60870@gmail.com>
2020-05-04 18:32:56 +0300

    Added name in parsedBlock

--- src/main/kotlin/org/bois/parser/ParsedBlockData.kt
+++ src/main/kotlin/org/bois/parser/ParsedBlockData.kt
@@ -1,6 +1,6 @@
 package org.bois.parser
 
-data class ParsedBlockData(val docs: ArrayList<String>, val header: String, val namespace: String?, val type: HeaderType?) {
+data class ParsedBlockData(val docs: ArrayList<String>, val header: String, val name: String?, val namespace: String?, val type: HeaderType?) {
     override fun toString(): String {
         val string = StringBuffer()
         docs.forEach { string.append(it) }
